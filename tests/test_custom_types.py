2bacead3d85f85e01fd410c52560591efea5747c
Ahmed Said <ahmed.said@jax.org>
2024-02-15 14:05:42 -0500

    initial commit with improved and restructured tests. Still missing models that involve platform-specific data

---
+++ tests/test_custom_types.py
@@ -0,0 +1,38 @@
+from scbl_db.custom_types import SamplesheetString, StrippedString
+from string import whitespace, punctuation
+from re import sub
+class TestStrippedString:
+    """
+    Tests for the `StrippedString` type.
+    """
+
+    def test_process_bind_param(self):
+        """
+        Test that the `process_bind_param` method strips whitespace from
+        the ends of the string but does not modify the string otherwise.
+        """
+        string = f'{whitespace}{punctuation}string{punctuation}{whitespace}'
+
+        assert (
+            StrippedString().process_bind_param(string=string, dialect='')
+            == f'{punctuation}string{punctuation}'
+        )
+
+
+class TestSamplesheetString:
+    """
+    Tests for the `SamplesheetString` type.
+    """
+
+    def test_process_bind_param(self):
+        """
+        Test that the `process_bind_param` method removes illegal
+        characters from the string.
+        """
+        illegal_punctuation = sub(pattern='[-_]', repl='', string=punctuation)
+        string = f'{illegal_punctuation}string{whitespace}string{illegal_punctuation}'
+
+        assert (
+            SamplesheetString().process_bind_param(string=string, dialect='')
+            == 'string-string'
+        )
---
+++ tests/test_models/fixtures.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from pytest import MonkeyPatch, fixture
+from scbl_db.models.entities import Institution, Lab, Person
+@fixture(scope='class')
+def institution() -> Institution:
+    """
+    Create a valid Institution object for testing.
+    """
+    return Institution(
+        name='Jackson Laboratory for Genomic Medicine',
+        short_name='JAX-GM',
+        email_format='{first_name}.{last_name}@jax.org',
+        country='US',
+        city='Farmington',
+        state='CT',
+    )
+@fixture(scope='class')
+def person(institution: Institution) -> Person:
+    """
+    Create a valid Person object for testing.
+    """
+    return Person(
+        first_name='ahmed',
+        last_name='said',
+        email='ahmed.said@jax.org',
+        orcid='0009-0008-3754-6150',
+        institution=institution,
+    )
+@fixture(scope='session')
+def delivery_parent_dir(monkeypatch: MonkeyPatch, tmp_path: Path) -> Path:
+    """
+    Create a temporary delivery parent directory for testing and set
+    the environment variable delivery_parent_dir to it. Also change
+    the return value of `pathlib.Path.group` to 'test_group' to avoid
+    messing with groups on the system.
+    """
+    delivery_parent_dir = tmp_path / 'delivery'
+    delivery_parent_dir.mkdir()
+
+    monkeypatch.setenv('delivery_parent_dir', str(delivery_parent_dir))
+    monkeypatch.setattr('pathlib.Path.group', lambda s: 'test_group')
+
+    return delivery_parent_dir
+@fixture(scope='class')
+def lab(delivery_parent_dir: Path, institution: Institution, person: Person) -> Lab:
+    """Create a valid Lab object for testing"""
+    (
+        delivery_parent_dir / f'{person.first_name.lower()}_{person.last_name.lower()}'
+    ).mkdir()
+    return Lab(institution=institution, pi=person)

2da59892d8f672357bfdb52d2023784b0d9d152f
Ahmed Said <ahmed.said@jax.org>
2024-02-26 18:12:41 -0500

    fix type hints for ORDERED_MODELS

--- scbl_db/__init__.py
+++ scbl_db/__init__.py
@@ -1,30 +1,10 @@
-from typing import TypedDict
-
 from .bases import Base
 from .models.entities import *
 from .models.platforms.chromium import *
 from .models.platforms.xenium import *
 from .models.processes import *
 
-
-class OrderedModelDict(TypedDict):
-    Institution: type[Institution]
-    Person: type[Person]
-    Lab: type[Lab]
-    Platform: type[Platform]
-    Assay: type[Assay]
-    ChromiumDataSet: type[ChromiumDataSet]
-    ChromiumTag: type[ChromiumTag]
-    ChromiumSample: type[ChromiumSample]
-    SequencingRun: type[SequencingRun]
-    ChromiumLibraryType: type[ChromiumLibraryType]
-    ChromiumLibrary: type[ChromiumLibrary]
-    XeniumRun: type[XeniumRun]
-    XeniumDataSet: type[XeniumDataSet]
-    XeniumSample: type[XeniumSample]
-
-
-ORDERED_MODELS: OrderedModelDict = {
+ORDERED_MODELS: dict[str, type[Base]] = {
     'Institution': Institution,
     'Person': Person,
     'Lab': Lab,
